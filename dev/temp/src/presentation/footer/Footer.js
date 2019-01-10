b07adaeb29acb2638376d018e09a63abec198a52
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2019-01-10 12:22:46 +0100

    Added link to eProcurement Ontology in footer

--- dev/temp/src/presentation/footer/Footer.js
+++ dev/temp/src/presentation/footer/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => (
     <Segment inverted vertical style={{ padding: '5em 0em', marginTop: '4rem' }}>
       <Container>
         <Grid divided inverted stackable>
-          <Grid.Row columns={3}>
+          <Grid.Row columns={4}>
             <Grid.Column>
               <Header inverted as='h4'>
                 <Translate id='footer.leftColumn'/>
@@ -31,11 +31,23 @@ const Footer = () => (
                 <List.Item as='a' href='http://www.areaprogrammabasento.it/' target='_blank'>
                     <Image src='/images/logo-areaprogrammabasento.png' size='small'/>
                 </List.Item>
-                <List.Item as='a' href='https://www.open-contracting.org/' target='_blank'>
-                    <Image src='/images/logo-open-contracting.png' size='small'/>
-                </List.Item>
               </List>
             </Grid.Column>
+            <Grid.Column>
+              <Header inverted as='h4'>
+                  <Translate id='footer.techColumn'/>
+                </Header>
+                <List link inverted>
+                  <List.Item as='a' href='https://www.open-contracting.org/' target='_blank'>
+                      <Image src='/images/logo-open-contracting.png' size='small'/>
+                  </List.Item>      
+                  <List.Item as='a' href='https://joinup.ec.europa.eu/solution/eprocurement-ontology/' target='_blank'>
+                      <Image src='/images/eprocurement.png' size='mini' style={{marginLeft:'15px', marginRight:'15px'}}/>
+                      <span>eProcurement Ontology</span>
+                  </List.Item>            
+                </List>
+            </Grid.Column>
+            
             <Grid.Column>
               <Header inverted as='h4'>
                 <Translate id='footer.rightColumn'/>
